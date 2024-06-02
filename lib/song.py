81717b46ef6197797b6bbf4308769e3c9c411e97
lema samuel <lemasam10@gmail.com>
2024-06-02 13:34:07 +0300

    completed assignment

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,47 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        # Instance attributes
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Increment count and add the song to genres and artists
+        Song.add_song_to_count()
+        Song.add_to_genres(self.genre)  # Pass the genre to the method
+        Song.add_to_artists(self.artist)  # Pass the artist to the method
+        Song.add_to_genre_count(self.genre)  # Pass the genre to the method
+        Song.add_to_artist_count(self.artist)  # Pass the artist to the method
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
